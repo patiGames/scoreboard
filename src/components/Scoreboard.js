0bb052620e2a1256ef5b3ece9094ea82c096dd2e
Enric Jordà <enric@pati.io>
2019-12-01 13:54:37 +0100

    Add functionality to ScoreBoard component

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import { initialState, setPoints } from '../gamePoints';
+import { getGameScore } from '../gameScoreFormatter';
 
 class Scoreboard extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  increasePoints(playerId) {
+    this.setState(setPoints(playerId, this.state));
+  }
+
   render() {
     return (
       <div>
         <h1>Tennis Scoreboard</h1>
-        <h2 id="score">Score: love-all</h2>
-        <button className="player1-scores" type="button">
+        <h2 id="score">Score: {getGameScore(this.state.gamePoints).scoreCall}</h2>
+        <button className="player1-scores"
+                type="button"
+                onClick={()=>this.increasePoints(1)}>
           Player 1 scores
         </button>
-        <button className="player2-scores" type="button">
+        <button className="player2-scores"
+                type="button"
+                onClick={()=>this.increasePoints(2)}>
           Player 2 scores
         </button>
       </div>
